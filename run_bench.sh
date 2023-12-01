9b2fd766bee578dd622958e759912d806b46fb5e
Prabhat Sharma <ptsharma@amazon.com>
2023-12-01 19:49:31 +0530

    run bench

--- run_bench.sh
+++ run_bench.sh
@@ -1,2 +1,3 @@
 mvn clean verify -DskipTests=true
-java -agentlib:jdwp=transport=dt_socket,address=*:8080,server=y,suspend=n -jar target/benchmarks.jar
+java -jar target/benchmarks.jar
+#java -agentlib:jdwp=transport=dt_socket,address=*:8080,server=y,suspend=n -jar target/benchmarks.jar
